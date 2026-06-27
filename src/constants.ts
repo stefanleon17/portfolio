febe2cbcf745982602820a41ec1b5e0a89d95852
Stefan Leon <stefan.leon17@gmail.com>
2026-06-27 18:21:54 +0300

    Adding deployment

--- src/constants.ts
+++ src/constants.ts
@@ -33,5 +33,5 @@ export const SITE_DATA = {
 
     // Base URL of your deployed site (no trailing slash), e.g. "https://your-username.github.io"
     // Used for Open Graph tags. Leave empty if you haven't deployed yet.
-    siteUrl: "",
+    siteUrl: "https://stefanleon17.github.io/portfolio",
 };
